dae2d22cc8e895dd9ca53e5a8ac46b1f70f2c616
scvgong <gongmyeongyun@gmail.com>
2023-12-15 11:03:50 +0900

    modifiy quest

--- docs/quests/solvingProblem_function.py
+++ docs/quests/solvingProblem_function.py
@@ -12,6 +12,7 @@ def connect(mongo_url,client,dbm): # connect 함수
     
     return collection
 
+# 데이터 함수
 def quiz_data():
     quiz_list = [
         {
@@ -56,10 +57,45 @@ def quiz_data():
 connect_data = connect(mongo_url,client,dbm)
 input_data = quiz_data()
 
+# DB에 데이터 입력 함수
 def insert_solving(connect_data,input_data):
     connect_data.insert_many(input_data)
 
-insert_solving(connect_data,input_data)
+# 퀴즈 함수
 
+def run_quiz(connect_data):
+    user_name = get_username() # 사용자 이름 입력
+    questions = connect_data.find() # DB에서 데이터 찾기
+    score = 0
+    
+    for idx,question in enumerate(questions,start=1): # 1부터 시작
+        print(f"{idx}.{question['question']}") #문제에 번호 붙여서 출력, list에 저장된 question 출력
 
+        for i, choice in enumerate(question['choices']):
+            print(f"{i+1}.{choice}") # 답항에 번호 붙여서 출력, list에 저장된 choices 출력
+        
+        answer = input("답을 입력해 주세요 : ")
+        if int(answer) == question['answer_number']: # 입력한 값이랑 DB에 저장된 번호 매칭
+            score += question['score'] # 점수 합산
+        
+    print("최종 점수 : {}".format(score) ) 
 
+    grade = ''
+    if score >= 80:
+        print("A 등급입니다.")
+        grade = 'A'
+    elif score >= 60:
+        print("B 등급입니다.")
+        grade = 'B'
+    else :
+        print("F등급입니다.")
+        grade = 'F'
+
+    insert_score(connect_data,user_name,score,grade) # DB에 추가 데이터 삽입
+
+def get_username():
+    user_name = input("사용자 이름을 입력 : ")
+    return user_name
+
+def insert_score(connect_data,username,score,grade): # 사용자가 입력한 값 DB에 저장
+    connect_data.insert_one({"username": username, "score":score, "grade":grade })
--- docs/quests/solvingProblem_main.py
+++ docs/quests/solvingProblem_main.py
@@ -1,30 +1,14 @@
-from pymongo import MongoClient
+import solvingProblem_function
 
 mongo_url = "mongodb://localhost:27017"
 client = "local"
 dbm = 'solvingProblem'
 
-def connect(mongo_url,client,dbm): # connect 함수
-    mongoClient = MongoClient(mongo_url)
-    database = mongoClient[client]
-    collection = database[dbm]
-    
-    return collection
+connect_data = solvingProblem_function.connect(mongo_url,client,dbm)
+input_data = solvingProblem_function.quiz_data()
 
-connect_data = connect(mongo_url,client,dbm)
+# 데이터 삽입
+solvingProblem_function.insert_solving(connect_data,input_data)
 
-
-
-# def quiz(connect_data):
-#     quizs = connect_data.find({},{"_id":0,'question':1})
-#     for question in quizs:
-#         answer = connect_data.find({},{"_id":0,"choices":1})
-#         for answer_correct in answer:
-#             print("1번 문제 : {}\n답항 : {}".format(question,answer_correct))
-
-# quiz(connect_data)
-
-# quiz_data => 데이터 함수
-# 답항 출력, 정답 번호 입력 받기
-
-# 문제 출력
+# 퀴즈 실행
+solvingProblem_function.run_quiz(connect_data)
